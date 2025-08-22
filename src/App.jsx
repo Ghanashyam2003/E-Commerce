81e6318c467aab375709bc14c19be881d57fcf18
ghanashyam2003 <shubhambadgujar2003@gmail.com>
2025-08-22 12:25:33 +0530

    Inital commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+ import React from 'react'
+ 
+ const App = () => {
+   return (
+     <div className='text-red-500 text-2xl font-bold'>
+        Hello, Vite with Tailwind CSS!
+     </div>
+   )
+ }
+ 
+ export default App
