58f97378f21984d5da8aa534d5bed5f5d38ff214
ghanashyam2003 <shubhambadgujar2003@gmail.com>
2025-08-26 12:49:01 +0530

    solve the netlify error

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,7 +4,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
-import Category from "./Category.jsx";
+import Category from "./Category";
 
 
 const Carousel = () => {
